9b9bceca922facf7fd8505f44a79f771d788c4e4
Flageat Valentin <valentin.flageat@etu.enseeiht.fr>
2020-04-28 16:37:47 +0200

    Fix deployment scripts

--- hidoop-clear.sh
+++ hidoop-clear.sh
@@ -24,6 +24,7 @@ else
 	DATA_FOLDER=$2
 fi 
 
+chmod u+x config/*.sh
 id=$1
 config/kill.sh $id
 config/clean.sh $id $DATA_FOLDER
--- hidoop-run.sh
+++ hidoop-run.sh
@@ -24,6 +24,7 @@ else
 	DATA_FOLDER=$2
 fi
 
+chmod u+x config/*.sh
 id=$1
 config/kill.sh $id
 config/exec.sh $id $DATA_FOLDER
