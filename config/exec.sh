8c4a50de70f08b2346ab1a4fae5c72b845126a66
leovct <l009.vincent@gmail.com>
2020-04-30 23:45:46 +0200

    ajout des améliorations sur les scripts

--- config/exec.sh
+++ config/exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 display_usage() {  
 	echo -e "usage : /config/exec.sh <serversloginID> [dataPathOnServers]" 
-	}
+}
 
 # check whether user had supplied -h or --help, if yes display usage 
 if [[ ( $# == "--help") ||  $# == "-h" ]] 
@@ -27,30 +27,44 @@ fi
 id=$1
 namenode=true
 input=config/servers
+cmd=""
+i=1
+nbrStart=$(< config/servers wc -l)
+nbrStart=$((nbrStart*2))
 while IFS= read -r line
 do
 	if [ "$namenode" = true ]
 	then
 		#Run NameNode
-		mate-terminal --window -t "NameNode $line" -e "ssh $id@$line java -classpath $DATA_FOLDER hdfs.NameNodeImpl"
+	        #mate-terminal --window -t "NameNode $line" -e "ssh $id@$line java -classpath $DATA_FOLDER hdfs.NameNodeImpl"
+		echo -e "\033[1;32m[$i/$nbrStart]\033[0m\033[1m starting NameNode on \e[4m$line\033[0m"
+		cmd+="mate-terminal --hide-menubar --window -t \"Namenode $line\" -e \"ssh $id@$line 'java -cp $DATA_FOLDER hdfs.NameNodeImpl'\""
 		namenode=false
 	else
 		#Run DataNode
- 		mate-terminal --tab -t "DataNode $line" -e "ssh $id@$line java -classpath $DATA_FOLDER hdfs.DataNodeImpl $line"
+ 	        #mate-terminal --tab -t "DataNode $line" -e "ssh $id@$line java -classpath $DATA_FOLDER hdfs.DataNodeImpl $line"
+		echo -e "\033[1;32m[$i/$nbrStart]\033[0m\033[1m starting DataNode on \e[4m$line\033[0m"
+		cmd+=" --tab -t \"DataNode $line\" -e \"ssh $id@$line 'sleep 5; java -cp $DATA_FOLDER hdfs.DataNodeImpl $line'\""
 	fi
+	i=$((i+1))
 done < "$input"
 
 index=0
 while IFS= read -r line
 do
-if [ $index = 1 ]
-then
+	if [ $index = 1 ]
+	then
 		#Run Daemon (new window)
-		mate-terminal --window -t "Daemon $line" -e "ssh $id@$line java -classpath $DATA_FOLDER ordo.DaemonImpl $line"
-elif [ $index -ge 2 ]
-then
+	        #mate-terminal --window -t "Daemon $line" -e "ssh $id@$line java -classpath $DATA_FOLDER ordo.DaemonImpl $line"
+		cmd+=" --window --hide-menubar -t \"Daemon $line\" -e \"ssh $id@$line 'java -cp $DATA_FOLDER ordo.DaemonImpl $line'\""
+	elif [ $index -ge 2 ]
+	then
 		#Run Daemon (new tab)
-		mate-terminal --tab -t "Daemon $line" -e "ssh $id@$line java -classpath $DATA_FOLDER ordo.DaemonImpl $line"
-fi
+	        #mate-terminal --tab -t "Daemon $line" -e "ssh $id@$line java -classpath $DATA_FOLDER ordo.DaemonImpl $line"
+		cmd+=" --tab -t \"Daemon $line\" -e \"ssh $id@$line 'java -cp $DATA_FOLDER ordo.DaemonImpl $line'\""
+	fi	
 	index=$(($index+1))
+	echo -e "\033[1;32m[$i/$nbrStart]\033[0m starting\033[1m Daemon on \e[4m$line\033[0m"
+	i=$((i+1))
 done < "$input"
+eval $cmd
--- config/ssh.sh
+++ config/ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 display_usage() {  
 	echo -e "usage : /config/ssh.sh <serversloginID>" 
-	}
+}
 
 # check whether user had supplied -h or --help, if yes display usage 
 if [[ ( $# == "--help") ||  $# == "-h" ]] 
@@ -19,12 +19,14 @@ fi
 
 id=$1
 
-#generate
-ssh-keygen -t rsa
-
 input=$PWD/config/servers
+i=1
+nbrAdd=$(< config/servers wc -l)
 while IFS= read -r line
 do
-	#copy
-	ssh-copy-id $id@$line
+	#add computer @line to the fingerprint
+	echo -e "\033[1;32m[$i/$nbrAdd]\033[0m\033[1m adding \e[4m$line\033[0m\033[1m to .ssh/known_hosts\033[0m"
+	ssh-keyscan -H $line >> ~/.ssh/known_hosts
+	# ssh -o StrictHostKeyChecking=no ?
+	i=$((i+1))
 done < "$input"
