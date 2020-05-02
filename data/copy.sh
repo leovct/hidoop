333f33105957f3f0040ebe1bfb26f2e3c02867c0
leovct <l009.vincent@gmail.com>
2020-05-02 15:59:36 +0200

    amelioration du script copy.sh

--- data/copy.sh
+++ data/copy.sh
@@ -3,6 +3,6 @@
 #copy i times
 for (( i=0; i<$1; i++ ))
 do 
-	echo "$i"
 	cat filesample.txt >> filesample$(($1)).txt
 done
+echo -e "\033[1;32m[copy done]\033[0m\033[1m filesample$1.txt generated (contains $1 times filesample.txt)"
